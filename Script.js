4c752805847537a4066af04a562dea525a9faee2
Lukey <lukemueller@charter.net>
2017-10-28 18:52:04 -0500

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-//document.write() - write on website
-//window.alert() - shows alert
-//console.log() - moves to websites console
-
-document.write("Hi");
-function OnClick() {
-   document.write("Ouch");
-}
